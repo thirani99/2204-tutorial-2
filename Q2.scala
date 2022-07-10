b8b8ebe0185653744d22d751884cf6ff9e0b3b8c
R.A.T.U.Perera <92754215+thirani99@users.noreply.github.com>
2022-07-10 12:34:10 +0530

    Create Q2.scala

---
+++ Q2.scala
@@ -0,0 +1,27 @@
+object main extends App{
+var g = 4.0f;
+var a :Int =2
+var b :Int =3
+var c :Int =4
+var d :Int =5
+var k :Float =4.3f
+
+b-=1 //--b
+println(b*a+c*d)
+d-=1 //d--
+
+println(a);
+a+=1//a++
+
+println(-2 *(g-k) + c)
+
+println(c)
+c+=1// c++
+
+c+=1//++c
+println(c*a)
+a+=1//a++
+
+
+
+}
